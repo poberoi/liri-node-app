2a80e5e814d54a7f39a9def99463bc8b20be0688
Puneet Oberoi <puneet.oberoi@outlook.com>
2016-07-07 12:35:19 -0400

    npm request installed

--- liri.js
+++ liri.js
@@ -1,3 +1,14 @@
 var keyList = require('./keys');
+var fs = require('fs');
+var request = require('request');
+var nodeArgvs = process.argv;
 
 console.log(keyList);
+
+fs.readFile("random.txt", "utf8", function(err, data){
+  var output = data.split(',');
+
+  for (var i=0; i<output.length; i++){
+    console.log(output[i]);
+  }
+});
